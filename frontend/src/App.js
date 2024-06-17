1446411e4c72ef806002b77884f7a52f5687e6aa
notMago <rodrigoandresvergaramoya@gmail.com>
2024-06-17 18:33:13 -0400

    The system startup successfully with Docker.

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a Cubremochilas</h1>
+        <p>
+          Esta es la interfaz de usuario donde los clientes interactúan con el sistema.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
